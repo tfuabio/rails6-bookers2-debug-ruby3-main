f4ee1989b17a9d5814e1ce49d6880e9ed908a03e
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-20 01:51:35 +0000

    [update] favoriteのcreate,destroyアクション

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root :to => "homes#top"
   get "home/about" => "homes#about"
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
-  resources :favorites, only: [:create, :destroy]
 end
