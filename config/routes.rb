c758fefb96aee67adf8d241371d1e4c59c00906d
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-22 02:30:36 +0000

    [update] Userのindex：フォロー・アンフォローリンク、フォロー・フォロワー数表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   root :to => "homes#top"
   get "home/about" => "homes#about"
   devise_for :users
@@ -6,5 +8,9 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
