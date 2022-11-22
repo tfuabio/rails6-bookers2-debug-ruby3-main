3760e835e8848e2bf79fcae2eca36138d5fdaa97
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-22 06:50:25 +0000

    [start] 応用課題５：検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  get 'search' => 'searches#search'
 end
