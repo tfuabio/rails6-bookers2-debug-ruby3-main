590008dc926b01fca97fdc3979e9f0d025071368
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-12-05 05:35:39 +0000

    [add] 投稿フォーム：Ajax非同期化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @books = Book.all # ajax対応用
     if @book.save
-      redirect_to book_path(@book.id), notice: "You have created book successfully."
+      #redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
-      @books = Book.all
       render 'index'
     end
   end
